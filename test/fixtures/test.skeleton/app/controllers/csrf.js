8effa7e9c6a73d9441326c97ce11ee31b020fe35
derdesign <der.kunstler@gmail.com>
2012-09-06 20:20:42 -0400

    Using app.session.load to load sessions in tests

--- test/fixtures/test.skeleton/app/controllers/csrf.js
+++ test/fixtures/test.skeleton/app/controllers/csrf.js
@@ -3,52 +3,46 @@ function CsrfController(app) {
   
   // Create csrf session data
   
-  // NOTE: Why is app.session.loadSession used ? See test/middleware/session.js
+  // Using a function stub to be able to register the extra callback on the
+  // route, since app.session.load is not available at this point in time.
+  var loadSession = function(req, res) {
+    return app.session.load(req, res);
+  }
   
-  get('/', function(req, res) {
-    app.session.loadSession(req, res, function() {
-      res.sendHeaders();
-      req.csrfToken('protect');
-      res.end('{CSRF INDEX}');
-    });
+  get('/', loadSession, function(req, res) {
+    res.sendHeaders();
+    req.csrfToken('protect');
+    res.end('{CSRF INDEX}');
   });
   
   // Check csrf data in session
-  get('/test', function(req, res) {
-    app.session.loadSession(req, res, function() {
-      res.json(req.session);
-    });
+  get('/test', loadSession, function(req, res) {
+    res.json(req.session);
   });
   
   // GET Csrf check, no params
   var cb1;
-  get('/check/get', cb1 = function(req, res) {
-    var self = this;
-    app.session.loadSession(req, res, function() {
-      self.getQueryData(req, 'protect', function(fields) {
-        res.json(fields);
-      });
+  get('/check/get', loadSession, cb1 = function(req, res) {
+    this.getQueryData(req, 'protect', function(fields) {
+      res.json(fields);
     });
   });
   
   // GET Csrf check, with query
   
-  get('/check/get/query', {name: 'alpha', age: 'integer'}, cb1);
+  get('/check/get/query', {name: 'alpha', age: 'integer'}, loadSession, cb1);
   
   // POST/PUT Csrf check, no params
   var cb2;
-  post('/check/post', cb2 = function(req, res) {
-    var self = this;
-    app.session.loadSession(req, res, function() {
-      self.getRequestData(req, 'protect', function(fields, files) {
-        res.json(fields);
-        files.removeAll();
-      });
+  post('/check/post', loadSession, cb2 = function(req, res) {
+    this.getRequestData(req, 'protect', function(fields, files) {
+      res.json(fields);
+      files.removeAll();
     });
   }, 'put');
   
   // POST/PUT Csrf check, with fields
-  post('/check/post/fields', {name: 'alpha', age: 'integer'}, cb2, 'put');
+  post('/check/post/fields', {name: 'alpha', age: 'integer'}, loadSession, cb2, 'put');
   
 }
 
--- test/fixtures/test.skeleton/app/controllers/session.js
+++ test/fixtures/test.skeleton/app/controllers/session.js
@@ -8,75 +8,69 @@ function SessionController(app) {
   // since it's loaded on its own test case. The simple way to do this, is
   // by adding the `app.session.load` as the first route callback
   
-  get('/', function(req, res) {
-    app.session.loadSession(req, res, function() {
-      res.sendHeaders();
-      res.end('{SESSION CONTROLLER}');
-    });
+  // Using a function stub to be able to register the extra callback on the
+  // route, since app.session.load is not available at this point in time.
+  var loadSession = function(req, res) {
+    return app.session.load(req, res);
+  }
+  
+  get('/', loadSession, function(req, res) {
+    res.sendHeaders();
+    res.end('{SESSION CONTROLLER}');
   });
   
-  get('/create/:user', {user: 'alpha'}, function(req, res, params) {
-    app.session.loadSession(req, res, function() {
-      var pers = req.queryData.persistent == '1';
-      app.session.create(req, res, {user: params.user}, pers, function(session, hashes, expires) {
-        app.globals.userSession = session;
-        res.sendHeaders({
-          'X-Session-Id': hashes.sessId,
-          'X-Session-Expires': (new Date(Date.now() + expires*1000)).toUTCString()
-        });
-        res.end(util.inspect(session));
+  get('/create/:user', {user: 'alpha'}, loadSession, function(req, res, params) {
+    var pers = req.queryData.persistent == '1';
+    app.session.create(req, res, {user: params.user}, pers, function(session, hashes, expires) {
+      app.globals.userSession = session;
+      res.sendHeaders({
+        'X-Session-Id': hashes.sessId,
+        'X-Session-Expires': (new Date(Date.now() + expires*1000)).toUTCString()
       });
+      res.end(util.inspect(session));
     });
   });
   
-  get('/destroy/:sid', {sid: 'md5_hash'}, function(req, res, params) {
-    app.session.loadSession(req, res, function() {
-      app.session.destroy(req, res, function() {
+  get('/destroy/:sid', {sid: 'md5_hash'}, loadSession, function(req, res, params) {
+    app.session.destroy(req, res, function() {
+      res.sendHeaders();
+      res.end('{SUCCESS}');
+    });
+  });
+  
+  get('/set/:varname/:value', {varname: 'alpha_dashes', value: 'alnum'}, loadSession, function(req, res, params) {
+    app.session.config.typecastVars.push(params.value); // automatically typecast
+    req.session[params.varname] = params.value;
+    res.sendHeaders();
+    res.httpMessage({
+      message: '{OK}',
+      raw: true
+    });
+  });
+  
+  get('/:action/:varname', {action: /^(get|delete)$/, varname: 'alpha_dashes'}, loadSession, function(req, res, params) {
+    switch(params.action) {
+      case 'get':
+        var ans = util.format('{%s}', req.session[params.varname] || '');
         res.sendHeaders();
-        res.end('{SUCCESS}');
-      });
-    });
-  });
-  
-  get('/set/:varname/:value', {varname: 'alpha_dashes', value: 'alnum'}, function(req, res, params) {
-    app.session.loadSession(req, res, function() {
-      app.session.config.typecastVars.push(params.value); // automatically typecast
-      req.session[params.varname] = params.value;
-      res.sendHeaders();
-      res.httpMessage({
-        message: '{OK}',
-        raw: true
-      });
-    });
-  });
-  
-  get('/:action/:varname', {action: /^(get|delete)$/, varname: 'alpha_dashes'}, function(req, res, params) {
-    app.session.loadSession(req, res, function() {
-      switch(params.action) {
-        case 'get':
-          var ans = util.format('{%s}', req.session[params.varname] || '');
-          res.sendHeaders();
-          res.end(ans);
-          break;
+        res.end(ans);
+        break;
 
-        case 'delete':
-          delete req.session[params.varname];
-          res.sendHeaders();
-          res.end('{OK}');
-          break;
-      }
-    });
+      case 'delete':
+        delete req.session[params.varname];
+        res.sendHeaders();
+        res.end('{OK}');
+        break;
+    }
   });
   
-  get('/incr/:varname', {varname: 'alpha_dashes'}, function(req, res, params) {
-    app.session.loadSession(req, res, function() {
-      res.sendHeaders();
-      if (req.session[params.varname] != null) {
-        req.session[params.varname]++;
-        res.end('{SUCCESS}');
-      }
-      else res.end('{FAIL}');
-    });
+  get('/incr/:varname', {varname: 'alpha_dashes'}, loadSession, function(req, res, params) {
+    res.sendHeaders();
+    if (req.session[params.varname] != null) {
+      req.session[params.varname]++;
+      res.end('{SUCCESS}');
+    }
+    else res.end('{FAIL}');
   });
   
 }
