a1f4d47450d62e2d58700316edba34322fb71031
derdesign <der.kunstler@gmail.com>
2012-09-14 09:49:53 -0400

    Added tests for refactored CSRF Middleware

--- test/fixtures/test.skeleton/app/controllers/csrf.js
+++ test/fixtures/test.skeleton/app/controllers/csrf.js
@@ -9,6 +9,34 @@ function CsrfController(app) {
     return app.session.load(req, res);
   }
   
+  get('/', loadSession, function(req, res) {
+    res.sendHeaders();
+    req.csrfToken('protect');
+    res.end('{CSRF INDEX}');
+  });
+  
+  // Check csrf data in session
+  get('/test', loadSession, function(req, res) {
+    res.json(req.session);
+  });
+  
+  // GET Csrf check
+  var cb1;
+  get('/check/get', loadSession, cb1 = function(req, res) {
+    req.getQueryData('protect', function(fields) {
+      res.json(fields);
+    });
+  });
+  
+  // POST/PUT Csrf check
+  var cb2;
+  post('/check/post', loadSession, cb2 = function(req, res) {
+    req.getRequestData('protect', function(fields, files) {
+      res.json(fields);
+      files.removeAll();
+    });
+  }, 'put');
+  
 }
 
 module.exports = CsrfController;
