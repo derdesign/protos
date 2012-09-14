26d4833c513074c9e6ecf9d588c236115af2a913
derdesign <der.kunstler@gmail.com>
2012-09-14 18:05:23 -0400

    Refactored Mailer middleware + Added tests

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -9,82 +9,71 @@
   » References:
   
     http://github.com/andris9/nodemailer
-
-  » SMTP Example:
-  
-    app.use('mailer', {
-      SMTP: {
-        host: 'smtp.example.com',     // required
-        port: 25,                     // optional, defaults to 25 or 465
-        use_authentication: false,    // optional, false by default
-        user: '',                     // used only when use_authentication is true 
-        pass: ''                      // used only when use_authentication is true
-      }
-    });
-  
-  » Amazon SES Example:
-  
-    app.use('mailer', {
-      SES: {
-        AWSAccessKeyID: 'ACCESSKEY',  // required
-        AWSSecretKey: 'SECRETKEY',    // required
-        ServiceUrl: 'https://email.us-east-1.amazonaws.com', // optional
-      }
-    });
-
-  » Sendmail Example (default):
-  
-    app.use('mailer', {
-      sendmail: true
-    });
     
-    app.use('mailer', {
-      sendmail: '/path/to/sendmail'
-    });
+  » Usage:
   
+    app.use('mailer');
+    
+    app.mailer.sendMail({
+      from: "Sender <name@domain.com>",
+      to: "receiver@example.com",
+      subject: "Hello",
+      text: "Hello World",
+      html: "<p>Hello World</p>"
+    }, function(err, res) {
+      doSomething();
+    });
+
  */
 
 var app = protos.app,
     util = require('util'),
     nodemailer = protos.requireDependency('nodemailer', 'Mailer Middleware', 'mailer'),
-    Application = app.constructor,
-    allowedTransports = ['SMTP', 'SES', 'sendmail'];
+    Application = app.constructor;
     
 // http://www.nodemailer.org/
 
 function Mailer(config, middleware) {
   
-  // Middleware configuration
-  var keys = Object.keys(config);
-  if (keys.length === 0) config = {sendmail: true};
+  // Set middleware instance
+  app[middleware] = this;
   
-  // Validate mail transports
-  if (keys.length > 1) {
-    // More than one transport configuration specified
-    throw new Error(util.format("Only one of the following allowed: %s", allowedTransports.join(', ')));
-  } else {
-    var transport = Object.keys(config).pop();
-    if (allowedTransports.indexOf(transport) >= 0) {
-      // Configure nodemailer transport
-      nodemailer[transport] = config[transport];
-    } else {
-      // Provided transport not supported
-      throw new Error(util.format("Mail transport not supported: '%s'. It should be one of: %s", 
-        transport, allowedTransports.join(', ')));
-    }
-  }
+  // Set configuration
+  this.config = protos.extend({
+    default: 'sendmail'
+  }, config);
+  
+  createTransports.call(this);
   
 }
 
 /**
-  Sends mail using the configured transport
+  Sends mail using the default transport
   
   @param {object} data
   @public
  */
 
-Application.prototype.sendmail = function(data) {
-  return nodemailer.send_mail(data);
+Mailer.prototype.sendMail = function(data, callback) {
+  return this.default.sendMail(data, callback);
+}
+
+function createTransports() {
+  
+  var cfg, transport;
+  
+  for (var t in this.config) {
+    cfg = this.config[t];
+    if (typeof cfg == 'string') {
+      cfg = [cfg, {}];
+    } else if (util.isArray(cfg)) {
+      if (cfg.length == 1) cfg.push({});
+    } else {
+      throw new Error("Mailer: array config expected");
+    }
+    this[t] = nodemailer.createTransport(cfg[0], cfg[1]);
+  }
+  
 }
 
 module.exports = Mailer;
---
+++ test/middleware/mailer.js
@@ -0,0 +1,43 @@
+
+var app = require('../fixtures/bootstrap.js'),
+    vows = require('vows'),
+    assert = require('assert');
+
+vows.describe('Mailer Middleware').addBatch({
+  
+  '': {
+    
+    topic: function() {
+      app.use('mailer', {
+        default: 'sendmail',
+        smtp: ['SMTP', {}],
+        other: ['SES'],
+      });
+      return app.mailer;
+    },
+    
+    "Properly configures transports": function(mailer) {
+      // default
+      assert.equal(mailer.default.transportType, 'SENDMAIL');
+      assert.equal(mailer.default.constructor.name, 'Transport');
+      assert.isFunction(mailer.default.sendMail);
+      
+      // smtp
+      assert.equal(mailer.smtp.transportType, 'SMTP');
+      assert.equal(mailer.smtp.constructor.name, 'Transport');
+      assert.isFunction(mailer.smtp.sendMail);
+      
+      // ses
+      assert.equal(mailer.other.transportType, 'SES');
+      assert.equal(mailer.other.constructor.name, 'Transport');
+      assert.isFunction(mailer.other.sendMail);
+    },
+    
+    "The sendMail method uses the default transport": function(mailer) {
+      assert.isFunction(app.mailer.sendMail);
+      assert.isTrue(app.mailer.sendMail.toString().indexOf('return this.default.sendMail(data, callback);') >= 0);
+    }
+
+  }
+  
+}).export(module);
