3d50ea45315a6c72c5706379db09f8c41c878147
derdesign <der.kunstler@gmail.com>
2012-11-11 11:06:40 -0400

    Removed install scripts (were creating problems)

--- tools/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install into $PREFIX/lib/node if running as root
-if [[ $EUID -eq 0 ]]; then
-	ln -sF "${PWD}" /usr/local/lib/node/protos
-fi
--- tools/scripts/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Uninstall from $PREFIX/lib/node if running as root
-if [[ $EUID -eq 0 ]]; then
-	unlink /usr/local/lib/node/protos &> /dev/null
-	exit 0
-fi
