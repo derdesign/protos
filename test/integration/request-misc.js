09b6a56d9dd3c5583520412f6af6e36d66f401ab
derdesign <der.kunstler@gmail.com>
2012-11-17 00:30:42 -0400

    Internals Refactor
    + Removed Application::_detectAjax
    + Moved AJAX detection to lib/request.js
    + Removed Application::_staticViewExists

--- test/integration/request-misc.js
+++ test/integration/request-misc.js
@@ -12,6 +12,34 @@ multi.on('post_exec', app.restoreFilters);
 
 vows.describe('Request Misc').addBatch({
   
+  'AJAX Detection': {
+    
+    topic: function() {
+      var promise = new EventEmitter();
+      
+      multi.curl('-i /detect-ajax');
+      multi.curl('-i -H "X-Requested-With: XMLHttpRequest" /detect-ajax');
+      
+      multi.exec(function(err, results) {
+        promise.emit('success', err || results);
+      });
+      
+      return promise;
+    },
+    
+    "Detects AJAX requests properly": function(results) {
+      var r1 = results[0],
+          r2 = results[1];
+      assert.isTrue(r1.indexOf('HTTP/1.1 200 OK') >= 0);
+      assert.isTrue(r1.indexOf('X-Ajax-Request: true') === -1); // Should not contain the header, it's not an ajax request
+      assert.isTrue(r2.indexOf('HTTP/1.1 200 OK') >= 0);
+      assert.isTrue(r2.indexOf('X-Ajax-Request: true') >= 0);   // Should contain the header, it's an ajax request
+    }
+    
+  }
+  
+}).addBatch({
+  
   'Referer URI': {
     
     topic: function() {
