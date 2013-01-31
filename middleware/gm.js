710c550eeb02a97af2b3ccc7013c89a6f9a8ab65
Ernesto Méndez <der.kunstler@gmail.com>
2013-01-30 22:52:31 -0400

    Complete rewrite of the GraphicsMagick middleware
    + Added the process() method
    + Added test cases

--- middleware/gm.js
+++ middleware/gm.js
@@ -5,17 +5,85 @@
   http://aheckmann.github.com/gm/
   http://www.graphicsmagick.org/
   
-  Provides an interface with the GraphicsMagick Library
-  
  */
 
 var app = protos.app;
 var gm = protos.requireDependency('gm', "GraphicsMagick Middleware");
 
+var Multi = protos.require('multi');
+
 function GraphicsMagick(config, middleware) {
   
-  app[middleware] = gm;
+  app[middleware] = this;
   
+}
+
+/**
+  Processes an image and stores into a target image
+  
+  The 'operations' object receives the method names
+  that the gm() object receives. There are special 
+  considerations when it comes to how the properties
+  map to the methods and how are arguments interpreted:
+  
+  Example:
+
+    app.gm.process({
+      in: 'rachel.jpg',
+      out: 'out.jpg',
+      operations: {
+        resize: [200, 0],
+        autoOrient: true,
+        sepia: true,
+        quality: 80
+      }
+    }, function(err) {
+      console.exit(err || 'success');
+    });
+
+  The example above does exactly the same as this:
+  
+    gm('rachel.jpg')
+      .resize(200, 0)
+      .autoOrient()
+      .sepia()
+      .quality(80)
+      .write('out.jpg', function(err) {
+        console.exit(err || 'success!');
+      });
+  
+  @param {object} ob Process Specification
+ */
+  
+
+GraphicsMagick.prototype.process = function(ob, callback) {
+  var inFile = ob.in;
+  var outFile = ob.out;
+  var operations = ob.operations || {};
+  var img = gm(inFile);
+  for (var cmd in operations) {
+    var args = operations[cmd];
+    img[cmd].apply(img, args instanceof Array ? args : (args === true) ? [] : [args]);
+  }
+  img.write(outFile, callback);
+}
+
+/**
+  Returns a multi object
+  
+  Note: This is really powerful when you want to
+  perform multiple image processing operations 
+  on parallel or sequentially.
+  
+  For the Multi configuration, refer to the
+  multi package at https://github.com/derdesign/multi
+  
+  @param {object} Multi Configuration
+  @return {object} Multi Instance
+ */
+
+GraphicsMagick.prototype.multi = function(cfg) {
+  return new Multi(this, cfg);
 }
 
 module.exports = GraphicsMagick;
--- test/middleware/gm.js
+++ test/middleware/gm.js
@@ -1,22 +1,52 @@
 
-var app = require('../fixtures/bootstrap'),
+var app = require('../fixtures/bootstrap');
+
+var fs = require('fs'),
     vows = require('vows'),
-    assert = require('assert');
+    assert = require('assert'),
+    EventEmitter = require('events').EventEmitter;
+    
+var Multi = protos.require('multi');
+
+var inFile = app.fullPath("priv/nodejs.png");
+var outFile = app.fullPath("priv/out.png");
 
 vows.describe('GraphicsMagick (middleware)').addBatch({
   
-  '': {
+  "": {
     
     topic: function() {
+      
+      var promise = new EventEmitter();
 
       app.use('gm');
       
-      return true;
+      app.gm.process({
+        in: inFile,
+        out: outFile,
+        operations: {
+          resize: [80, 0],      // Tests multiple arguments
+          sepia: true,          // Tests no arguments
+          quality: 80           // Tests single argument
+        }
+      }, function(err) {
+        promise.emit('success', err || null);
+      });
+      
+      return promise;
       
     },
     
-    "Binds the gm module to app.gm": function() {
-      assert.isTrue(app.gm === protos.require('gm'));
+    "Properly converts images": function(err) {
+      assert.isNull(err);
+      assert.isTrue(fs.existsSync(outFile));
+      fs.unlink(outFile);
+    },
+
+    "Returns valid Multi object": function(err) {
+      var multi = app.gm.multi();
+      assert.isTrue(multi instanceof Multi);
+      assert.isTrue(multi.process instanceof Function);
     }
     
   }
