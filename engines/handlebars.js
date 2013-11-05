5a7264500e0fde2c48bccb82aaaec2cfb07d8244
Ernesto Méndez <der.kunstler@gmail.com>
2013-11-05 16:51:55 -0400

    Fix issue that prevented handlebars partials from being updated

--- engines/handlebars.js
+++ engines/handlebars.js
@@ -87,18 +87,19 @@ app.on('view_partials_loaded', function(ob) {
   var slice = Array.prototype.slice;
   
   Object.keys(ob).forEach(function(name) {
-    var func = ob[name];
     if (name[0] == '$') { // Helpers
       optionsContext.helpers[name.slice(1)] = function() {
         var args = slice.call(arguments, 0);
         var options = args.pop();
         var params = options.hash;
         var content = options.fn instanceof Function ? options.fn(this) : '';
-        var out = func.apply(null, [content].concat(args).concat([params]));
+        var out = ob[name].apply(null, [content].concat(args).concat([params]));
         return new SafeString(out);
       }
     } else { // Partials
-      optionsContext.partials[name] = ob[name]; // Can be passed as-is
+      optionsContext.partials[name] = function() {
+        return ob[name].apply(null, arguments);
+      };
     }
   });
   
